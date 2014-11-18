90ff3947d106a6fe5cf93b91da1bb8a4876de118
Naugtur <naugtur@gmail.com>
2014-11-18 19:00:52 +0100

    minification

--- gruntfile.js
+++ gruntfile.js
@@ -16,14 +16,36 @@ module.exports = function (grunt) {
                     }
                 }
             }
+        },
+
+        
+        uglify: {
+            options: {
+                report: "gzip", //only shows with --verbose in current version
+                banner: "// <%= pkg.name %> v<%= pkg.version %> \n" +
+                    "// license:<%= pkg.license %> \n" +
+                    "// <%= pkg.author %> \n" +
+                    "// built <%= grunt.template.today('yyyy-mm-dd') %> \n" //remember the newline! :)
+            },
+            dist: {
+                files: [{
+                    src: 'dist/*.js', // source files mask
+                    dest: 'dist/', // destination folder
+                    expand: true, // allow globbing src
+                    flatten: true, // remove all unnecessary nesting
+                    ext: '.min.js' // replace .js to .min.js
+                }]
+            }
         }
+
     });
 
     grunt.loadNpmTasks("grunt-browserify");
     grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
-    grunt.registerTask("build", ["clean", "browserify"]);
-    
+    grunt.registerTask("build", ["clean", "browserify", "uglify"]);
+
     grunt.registerTask("default", ["build"]);
 
 }
